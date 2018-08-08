b2d6232a8d2782e7f126bd35fa511deddd3aae31
Bruno Krebs <bruno.krebs@auth0.com>
2018-08-08 19:19:53 -0300

    Moving sensitive information to the main process.

--- frontend/main.js
+++ frontend/main.js
@@ -13,8 +13,6 @@ let accessToken = null;
 let refreshToken = null;
 let idToken = null;
 
-let win;
-
 // you will need a custom scheme so Auth0 can redirect to this scheme+domain after authentication
 // i.e. `file://home.html` won't work because Auth0 appends a hash to the callback URL and Electron
 // will think it has to load a file called (e.g.) `home.html#access_token=123...`
@@ -29,65 +27,109 @@ protocol.registerStandardSchemes([customScheme]);
 function showWindow() {
 
   protocol.registerFileProtocol(customScheme, (req, callback) => {
-    const urlRequested = req.url.replace(`${customScheme}://${customDomain}/`, '').substring(0, req.url.length - 1);
+    const requestedURL = req.url.replace(`${customScheme}://${customDomain}/`, '').substring(0, req.url.length - 1);
 
-    if (urlRequested.indexOf('callback') === 0) {
-      win.close();
-      const urlParts = url.parse(urlRequested, true);
-      const query = urlParts.query;
-
-      const exchangeOptions = {
-        'grant_type': 'authorization_code',
-        'client_id': clientId,
-        'code_verifier': verifier,
-        'code': query.code,
-        'redirect_uri': redirectUri,
-      };
-
-      const options = {
-        method: 'POST',
-        url: `https://${auth0Domain}/oauth/token`,
-        headers: {
-          'content-type': 'application/json'
-        },
-        body: JSON.stringify(exchangeOptions),
-      };
-
-      request(options, function (error, resp, body) {
-        if (error) throw new Error(error);
-
-        win = new BrowserWindow({
-          width: 1000,
-          height: 600,
-        });
-
-        const responseBody = JSON.parse(body);
-        accessToken = responseBody.access_token;
-        idToken = responseBody.id_token;
-        refreshToken = responseBody.refresh_token;
-
-        ipcMain.on('loaded', () => {
-          win.webContents.send('globalProps', {
-            idToken,
-          });
-        });
-
-        win.loadURL(`${customScheme}://${customDomain}/home.html`);
-      });
-
-      return;
+    if (requestedURL.indexOf('callback') === 0) {
+      return triggerHome(requestedURL);
     }
 
-    console.log('here i am -------------------------------------');
-    console.log(`${__dirname}/renderer/${urlRequested}`);
-    callback(`${__dirname}/renderer/${urlRequested}`);
-    console.log('here i am -------------------------------------');
-  }, (error) => {
-    if (error) console.error('Failed to register protocol')
-  });
+    callback(`${__dirname}/renderer/${requestedURL}`);
+  }, console.error);
 
+  triggerAuthentication();
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', showWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    showWindow();
+  }
+});
+
+function triggerHome(requestedURL) {
+  const urlParts = url.parse(requestedURL, true);
+  const query = urlParts.query;
+
+  const exchangeOptions = {
+    'grant_type': 'authorization_code',
+    'client_id': clientId,
+    'code_verifier': verifier,
+    'code': query.code,
+    'redirect_uri': redirectUri,
+  };
+
+  const options = {
+    method: 'POST',
+    url: `https://${auth0Domain}/oauth/token`,
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(exchangeOptions),
+  };
+
+  request(options, function (error, resp, body) {
+    if (error) throw new Error(error);
+
+    const win = new BrowserWindow({
+      width: 1000,
+      height: 600,
+    });
+
+    const responseBody = JSON.parse(body);
+    accessToken = responseBody.access_token;
+    idToken = responseBody.id_token;
+    refreshToken = responseBody.refresh_token;
+
+    ipcMain.on('loaded', (event) => {
+      event.sender.send('load-profile', {
+        idToken,
+      });
+    });
+
+    ipcMain.on('secured-request', (event) => {
+      const requestOptions = {
+        method: 'GET',
+        url: `http://localhost:3000/private`,
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+        },
+      };
+
+      request(requestOptions, function (error, resp, body) {
+        if (error) throw new Error(error);
+
+        event.sender.send('secured-request-response', {
+          message: resp.body,
+        });
+      });
+    });
+
+    ipcMain.on('logout', (event) => {
+      event.sender.close();
+    });
+
+    win.loadURL(`${customScheme}://${customDomain}/home.html`);
+  });
+}
+
+function triggerAuthentication() {
   // Create the browser window.
-  win = new BrowserWindow({
+  const win = new BrowserWindow({
     width: 1000,
     height: 600,
     webPreferences: {
@@ -118,31 +160,9 @@ function showWindow() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null;
+    // win = null;
   });
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', showWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    showWindow();
-  }
-});
 
 function base64URLEncode(str) {
   return str.toString('base64')
--- frontend/renderer/home.js
+++ frontend/renderer/home.js
@@ -1,40 +1,25 @@
-const axios = require('axios');
 const { ipcRenderer } = require('electron');
 const jwtDecode = require('jwt-decode');
 
 ipcRenderer.send('loaded');
 
-ipcRenderer.on('globalProps', (event, props) => {
-  console.log('----------------------');
+document.getElementById('logout').onclick = () => {
+  ipcRenderer.send('logout');
+};
+
+document.getElementById('secured-request').onclick = () => {
+  ipcRenderer.send('secured-request');
+};
+
+ipcRenderer.on('load-profile', (event, props) => {
   const {idToken} = props;
-  populateView(idToken, true);
+  const profile = jwtDecode(idToken);
 
-  function populateView(idToken) {
-    console.log(idToken);
-    const profile = jwtDecode(idToken);
+  document.getElementById('picture').src = profile.picture;
+  document.getElementById('name').innerText = profile.name;
+  document.getElementById('success').innerText = 'The `access_token` was loaded with the authorization code.';
+});
 
-    document.getElementById('picture').src = profile.picture;
-    document.getElementById('name').innerText = profile.name;
-    document.getElementById('success').innerText = 'The `access_token` was loaded with the authorization code.';
-    document.getElementById('logout').onclick = logout;
-  }
-
-  function logout() {
-    localStorage.clear();
-    window.location.assign(`${customScheme}://${customDomain}/index.html`);
-  }
-
-  function fetchPrivate() {
-    const options = {
-      method: 'GET',
-      headers: { 'Authorization': 'Bearer ' +  sessionStorage.getItem('access_token') },
-      url: 'http://localhost:3000/private',
-    };
-
-    axios(options).then((response) => {
-      alert(response.data);
-    }).catch(console.log);
-  }
-
-  document.getElementById('private').onclick = fetchPrivate;
+ipcRenderer.on('secured-request-response', (event, props) => {
+  document.getElementById('message').innerText = props.message;
 });
